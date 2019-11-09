722d4ed3d766210c7a19f1bb5248d7298626ed22
nicolaskobelt <kobeltnicolas156@gmail.com>
2019-11-09 14:04:21 -0300

    deploy test4

--- src/App.vue
+++ src/App.vue
@@ -1,50 +1,42 @@
 <template>
   <v-app>
-    <v-app-bar
-      app
-      color="gray"
-    >
+    <v-app-bar app color="gray">
       <div class="d-flex align-center">
-        <v-btn
-          icon
-          color="black"
-          @click.stop="drawer = !drawer"
-          class="hidden-sm-and-up"
-        >
-        <v-icon>fas fa-bars</v-icon>
+        <v-btn icon color="black" @click.stop="drawer = !drawer" class="hidden-sm-and-up">
+          <v-icon>fas fa-bars</v-icon>
         </v-btn>
       </div>
 
       <v-icon></v-icon>
       <v-toolbar-items v-bind:key="item" v-for="item in items" class="hidden-xs-only">
         <v-btn text @click="$vuetify.goTo(item.target)">
-            <v-icon left class="cyan--text text--dark-6">{{ item.icon }}</v-icon>
-            {{item.title}}
+          <v-icon left class="cyan--text text--dark-6">{{ item.icon }}</v-icon>
+          {{item.title}}
         </v-btn>
-
       </v-toolbar-items>
 
       <v-spacer></v-spacer>
-
     </v-app-bar>
 
-    <v-navigation-drawer
-      v-model="drawer"
-      absolute
-      temporary
-    >
-      <v-list dense>
+    <v-navigation-drawer v-model="drawer" fixed temporary>
+      <v-list-item>
+        <v-list-item-avatar>
+          <v-img src="./assets/profile128X128.png"></v-img>
+        </v-list-item-avatar>
 
-        <v-list-item
-          v-for="item in items"
-          :key="item.title"
-          link
-        >
+        <v-list-item-content>
+          <v-list-item-title>Nicolás</v-list-item-title>
+        </v-list-item-content>
+      </v-list-item>
+
+      <v-divider></v-divider>
+      <v-list dense>
+        <v-list-item v-for="item in items" :key="item.title" link>
           <v-list-item-icon>
             <v-icon class="cyan--text text--dark-6">{{ item.icon }}</v-icon>
           </v-list-item-icon>
 
-          <v-list-item-content @click="$vuetify.goTo(item.target)">
+          <v-list-item-content @click="$vuetify.goTo(item.target), drawer = !drawer">
             <v-list-item-title>{{ item.title }}</v-list-item-title>
           </v-list-item-content>
         </v-list-item>
@@ -52,31 +44,43 @@
     </v-navigation-drawer>
 
     <v-content>
-      <HelloWorld/>
+      <HelloWorld />
     </v-content>
+
+  <v-card height="50">
+    <v-footer
+      absolute
+      class="font-weight-medium"
+    >
+      <v-col
+        class="text-center"
+        cols="12"
+      >
+        {{ new Date().getFullYear() }} — <strong>By Nicolás Kobelt made with Vuetify</strong>
+      </v-col>
+    </v-footer>
+  </v-card>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
+import HelloWorld from "./components/HelloWorld";
 
 export default {
-  name: 'App',
+  name: "App",
 
   components: {
-    HelloWorld,
+    HelloWorld
   },
 
   data: () => ({
-        drawer: null,
-        items: [
-          { title: 'About me', icon: 'fas fa-user-alt', target: '#about'},
-          { title: 'Experience', icon: 'fas fa-code-branch',target: '#experience' },
-          { title: 'Education', icon: 'fas fa-book-open',target: '#education'},
-          { title: 'Skills', icon: 'fas fa-user-tag',target: '#skills' },
-          
-        ],
-    //
-  }),
+    drawer: null,
+    items: [
+      { title: "About me", icon: "fas fa-user-alt", target: "#about" },
+      { title: "Experience", icon: "fas fa-code-branch", target: "#experience"},
+      { title: "Education", icon: "fas fa-book-open", target: "#education" },
+      { title: "Skills", icon: "fas fa-user-tag", target: "#skills" }
+    ]
+  })
 };
 </script>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,231 +1,180 @@
 <template>
-<v-content style="padding: 0px 0px 0px;">
- <v-parallax src="https://cdn.vuetifyjs.com/images/parallax/material.jpg">
- <div class="mx-auto">
- <v-img src="../assets/profile128X128.png"
-    class="center center"
-    contain
-    width="120px"
-    height="100px"></v-img>
-  </div>
- <h2 class="white--text mb-2 display-2 text-center">Nicolás Kobelt</h2>
- <h2 class="white--text mb-2 text-center">Student of Systems Engineering</h2>
-<div class="mx-auto">
-    <v-row justify="space-around">
-      <v-btn text dark href="https://www.linkedin.com/in/nicolaskobelt/">
-        <v-icon dark>fab fa-linkedin</v-icon>
-      </v-btn>
-      <v-btn text dark href="https://github.com/nicolaskobelt">
-        <v-icon dark>fab fa-github</v-icon>
-      </v-btn>
-      <v-btn text dark href="mailto:nicolaskobelt@gmail.com">
-        <v-icon dark>fas fa-envelope</v-icon>
-      </v-btn>
-      <v-btn text dark href="https://t.me/nicolaskobelt">
-        <v-icon dark>fab fa-telegram</v-icon>
-      </v-btn>
-      
+  <v-content style="padding: 0px 0px 0px;">
+    <v-parallax src="https://cdn.vuetifyjs.com/images/parallax/material.jpg">
+      <div class="mx-auto">
+        <v-img
+          src="../assets/profile128X128.png"
+          class="center center"
+          contain
+          width="120px"
+          height="100px"
+        ></v-img>
+      </div>
+      <h2 class="white--text mb-2 display-2 text-center">Nicolás Kobelt</h2>
+      <h2 class="white--text mb-2 text-center">Student of Systems Engineering</h2>
+      <div class="mx-auto">
+        <v-row justify="space-around">
+          <v-btn text dark href="https://www.linkedin.com/in/nicolaskobelt/">
+            <v-icon dark>fab fa-linkedin</v-icon>
+          </v-btn>
+          <v-btn text dark href="https://github.com/nicolaskobelt">
+            <v-icon dark>fab fa-github</v-icon>
+          </v-btn>
+          <v-btn text dark href="mailto:nicolaskobelt@gmail.com">
+            <v-icon dark>fas fa-envelope</v-icon>
+          </v-btn>
+          <v-btn text dark href="https://t.me/nicolaskobelt">
+            <v-icon dark>fab fa-telegram</v-icon>
+          </v-btn>
+        </v-row>
+      </div>
+    </v-parallax>
 
-     
-    </v-row>
-</div>
- </v-parallax>
-
-<section>
-        <v-layout
-          column
-          align-center
-        >
-          <v-flex xs12 sm4 class="my-5">
-            <div class="text-center" id='about'>
-              <v-icon class="cyan--text text--dark-2">fas fa-user-alt</v-icon>
-              <h2 class="headline">About me</h2>
-            </div>
-          </v-flex>
-          <v-flex>
-            <v-container>
-              <v-layout align-center>
-                <v-flex class="text-justify">
-                    <v-card-text class="text-justify">
-                      I am student in the last year of systems engineering, 
-                      I love this career, this help me found what is my passion. <br>
-                      I came from a little town in Córdoba, Argentina. 
-                      Actually I am living in Córdoba City. 
-                      What I said, the career help me found what are my passion, 
-                      this are Artificial Intelligence, Data Sciene, App develop, etc.
-                      I can work in teams, I like know new people, 
-                      learn from them and try understand each one. 
-                      I like do fisical activities, go out with friends, enjoy. 
-                    </v-card-text>
-                </v-flex>
-              </v-layout>
-            </v-container>
-          </v-flex>
-        </v-layout>
-      </section>
-
-<section>
-        <v-layout
-          column
-          align-center
-        >
-          <v-flex xs12 sm4 class="my-5">
-            <div class="text-center" id='experience'>
-              <v-icon class="cyan--text text--dark-2">fas fa-code-branch</v-icon>
-              <h2 class="headline">Experience</h2>
-            </div>
-          </v-flex>
-          <v-flex>
-            <v-container>
-                <v-list-item justify="center">
-                  <v-list-item-avatar>
-                    <v-img src="../assets/checkbox.png"
-                            class="center center"
-                            contain
-                            width="120px"
-                            height="100px">
-                            </v-img>
-                  </v-list-item-avatar>
-                  <v-list-item-content>
-                    <v-list-item-title class="headline">Software Developer</v-list-item-title>
-                    <v-list-item-subtitle>Checkbox Innovations</v-list-item-subtitle>
-                    <v-list-item-subtitle>October 2019 - Present</v-list-item-subtitle>
-                  </v-list-item-content>
-                </v-list-item>
-            </v-container>
-          </v-flex>
-        </v-layout>
-      </section>
-
-
-      <section>
-        <v-layout
-          column
-          align-center
-        >
-          <v-flex xs12 sm4 class="my-5">
-            <div class="text-center" id="education">
-              <v-icon class="cyan--text text--dark-2">fas fa-book-open</v-icon>
-              <h2 class="headline">Education</h2>
-            </div>
-          </v-flex>
-          <v-flex>
-            <v-container>
-                <v-list-item justify="center">
-                  <v-list-item-avatar>
-                    <v-img src="../assets/logo-ucc-2018.png"
-                            class="center center"
-                            contain
-                            width="120px"
-                            height="100px">
-                            </v-img>
-                  </v-list-item-avatar>
-                  <v-list-item-content>
-                    <v-list-item-title class="headline">Systems Engineering</v-list-item-title>
-                    <v-list-item-subtitle>Universidad Católica de Córdoba</v-list-item-subtitle>
-                    <v-list-item-subtitle>March 2015 - Present</v-list-item-subtitle>
-                  </v-list-item-content>
-                </v-list-item>
-            </v-container>
-          </v-flex>
-
-
-      <v-flex>
-        <v-container>
-                <v-list-item justify="center">
-                  <v-list-item-avatar>
-                    <v-img src="../assets/logo-ucc-2018.png"
-                            class="center center"
-                            contain
-                            width="120px"
-                            height="100px">
-                            </v-img>
-                  </v-list-item-avatar>
-                  <v-list-item-content>
-                    <v-list-item-title class="headline">Project Management Diploma</v-list-item-title>
-                    <v-list-item-subtitle>Universidad Católica de Córdoba</v-list-item-subtitle>
-                    <v-list-item-subtitle>June 2019 - October 2019</v-list-item-subtitle>
-                  </v-list-item-content>
-                </v-list-item>
-            </v-container>
-          </v-flex>
-
-      <v-flex>
-        <v-container>
-                <v-list-item justify="center">
-                  <v-list-item-avatar color="white">
-      
-                  </v-list-item-avatar>
-                  <v-list-item-content>
-                    <v-list-item-title class="headline">Bachelor in Economics and Management </v-list-item-title>
-                    <v-list-item-subtitle>Instituto Parroquial San Juan Diego</v-list-item-subtitle>
-                    <v-list-item-subtitle>March 2009 - December 2014</v-list-item-subtitle>
-                  </v-list-item-content>
-                </v-list-item>
-            </v-container>
-          </v-flex>
-
+    <section>
+      <v-layout column align-center>
+        <v-flex xs12 sm4 class="my-5">
+          <div class="text-center" id="about">
+            <v-icon class="cyan--text text--dark-2">fas fa-user-alt</v-icon>
+            <h2 class="headline">About me</h2>
+          </div>
+        </v-flex>
+        <v-flex>
+          <v-container>
+            <v-layout align-center>
+              <v-flex class="text-justify">
+                <v-card-text class="text-justify">
+                  I am student in the last year of systems engineering,
+                  I love this career, this help me found what is my passion.
+                  <br />I came from a little town in Córdoba, Argentina.
+                  Actually I am living in Córdoba City.
+                  What I said, the career help me found what are my passion,
+                  this are Artificial Intelligence, Data Sciene, App develop, etc.
+                  I can work in teams, I like know new people,
+                  learn from them and try understand each one.
+                  I like do fisical activities, go out with friends, enjoy.
+                </v-card-text>
+              </v-flex>
+            </v-layout>
+          </v-container>
+        </v-flex>
       </v-layout>
     </section>
 
-</v-content>
+    <section>
+      <v-layout column align-center>
+        <v-flex xs12 sm4 class="my-5">
+          <div class="text-center" id="experience">
+            <v-icon class="cyan--text text--dark-2">fas fa-code-branch</v-icon>
+            <h2 class="headline">Experience</h2>
+          </div>
+        </v-flex>
+        <v-flex>
+          <v-container>
+            <v-list-item justify="center">
+              <v-list-item-avatar>
+                <v-img
+                  src="../assets/checkbox.png"
+                  class="center center"
+                  contain
+                  width="120px"
+                  height="100px"
+                ></v-img>
+              </v-list-item-avatar>
+              <v-list-item-content>
+                <v-list-item-title class="headline">Software Developer</v-list-item-title>
+                <v-list-item-subtitle>Checkbox Innovations</v-list-item-subtitle>
+                <v-list-item-subtitle>October 2019 - Present</v-list-item-subtitle>
+              </v-list-item-content>
+            </v-list-item>
+          </v-container>
+        </v-flex>
+      </v-layout>
+    </section>
+
+    <section>
+      <v-layout column align-center>
+        <v-flex xs12 sm4 class="my-5">
+          <div class="text-center" id="education">
+            <v-icon class="cyan--text text--dark-2">fas fa-book-open</v-icon>
+            <h2 class="headline">Education</h2>
+          </div>
+        </v-flex>
+        <v-flex>
+          <v-container>
+            <v-list-item justify="center">
+              <v-list-item-avatar>
+                <v-img
+                  src="../assets/logo-ucc-2018.png"
+                  class="center center"
+                  contain
+                  width="120px"
+                  height="100px"
+                ></v-img>
+              </v-list-item-avatar>
+              <v-list-item-content>
+                <v-list-item-title class="headline">Systems Engineering</v-list-item-title>
+                <v-list-item-subtitle>Universidad Católica de Córdoba</v-list-item-subtitle>
+                <v-list-item-subtitle>March 2015 - Present</v-list-item-subtitle>
+              </v-list-item-content>
+            </v-list-item>
+          </v-container>
+        </v-flex>
+
+        <v-flex>
+          <v-container>
+            <v-list-item justify="center">
+              <v-list-item-avatar>
+                <v-img
+                  src="../assets/logo-ucc-2018.png"
+                  class="center center"
+                  contain
+                  width="120px"
+                  height="100px"
+                ></v-img>
+              </v-list-item-avatar>
+              <v-list-item-content>
+                <v-list-item-title class="headline">Project Management Diploma</v-list-item-title>
+                <v-list-item-subtitle>Universidad Católica de Córdoba</v-list-item-subtitle>
+                <v-list-item-subtitle>June 2019 - October 2019</v-list-item-subtitle>
+              </v-list-item-content>
+            </v-list-item>
+          </v-container>
+        </v-flex>
+
+        <v-flex>
+          <v-container>
+            <v-list-item justify="center">
+              <v-list-item-avatar color="white"></v-list-item-avatar>
+              <v-list-item-content>
+                <v-list-item-title class="headline">Bachelor in Economics and Management</v-list-item-title>
+                <v-list-item-subtitle>Instituto Parroquial San Juan Diego</v-list-item-subtitle>
+                <v-list-item-subtitle>March 2009 - December 2014</v-list-item-subtitle>
+              </v-list-item-content>
+            </v-list-item>
+          </v-container>
+        </v-flex>
+      </v-layout>
+    </section>
+
+
+    <section>
+      <v-layout column align-center>
+        <v-flex xs12 sm4 class="my-5">
+          <div class="text-center" id="skills">
+            <v-icon class="cyan--text text--dark-2">fas fa-user-tag</v-icon>
+            <h2 class="headline">Skills</h2>
+          </div>
+        </v-flex>
+      </v-layout>
+    </section>
+
+  </v-content>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
+  name: "HelloWorld",
 
-  data: () => ({
-    ecosystem: [
-      {
-        text: 'vuetify-loader',
-        href: 'https://github.com/vuetifyjs/vuetify-loader',
-      },
-      {
-        text: 'github',
-        href: 'https://github.com/vuetifyjs/vuetify',
-      },
-      {
-        text: 'awesome-vuetify',
-        href: 'https://github.com/vuetifyjs/awesome-vuetify',
-      },
-    ],
-    importantLinks: [
-      {
-        text: 'Documentation',
-        href: 'https://vuetifyjs.com',
-      },
-      {
-        text: 'Chat',
-        href: 'https://community.vuetifyjs.com',
-      },
-      {
-        text: 'Made with Vuetify',
-        href: 'https://madewithvuejs.com/vuetify',
-      },
-      {
-        text: 'Twitter',
-        href: 'https://twitter.com/vuetifyjs',
-      },
-      {
-        text: 'Articles',
-        href: 'https://medium.com/vuetify',
-      },
-    ],
-    whatsNext: [
-      {
-        text: 'Explore components',
-        href: 'https://vuetifyjs.com/components/api-explorer',
-      },
-      {
-        text: 'Select a layout',
-        href: 'https://vuetifyjs.com/layout/pre-defined',
-      },
-      {
-        text: 'Frequently Asked Questions',
-        href: 'https://vuetifyjs.com/getting-started/frequently-asked-questions',
-      },
-    ],
-  }),
+  data: () => ({})
 };
 </script>
