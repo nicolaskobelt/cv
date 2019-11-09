a0fef1cc4538707d2a9b9575dd22fdb237a91fae
nicolaskobelt <kobeltnicolas156@gmail.com>
2019-11-09 13:35:59 -0300

    deploy test 3

--- server.js
+++ server.js
@@ -1,13 +1,16 @@
 const express = require('express')
-const serveStatic = require("serve-static")
+const serveStatic = require('serve-static')
 const path = require('path')
 
+const app = express()
 
-app = express();
-app.use(serveStatic(path.join(__dirname, 'dist')));
-app.use(express.static(__dirname + '/public'));
-app.use(function(req, res) {
-    res.sendFile(path.join(__dirname, '/public', 'index.html'));
-});
-// const port = ;
-app.listen(process.env.PORT || 3000);
+//here we are configuring dist to serve app files
+app.use('/', serveStatic(path.join(__dirname, '/dist')))
+
+// this * route is to serve project on different page routes except root `/`
+app.get(/.*/, function (req, res) {
+	res.sendFile(path.join(__dirname, '/dist/index.html'))
+})
+
+const port = process.env.PORT || 8080
+app.listen(port)
