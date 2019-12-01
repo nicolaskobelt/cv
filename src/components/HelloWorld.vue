3ff6a9799ca23e08e5081afcc3fafa0abcf1b20c
nicolaskobelt <kobeltnicolas156@gmail.com>
2019-12-01 16:49:10 -0300

    final

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -14,16 +14,16 @@
       <h2 class="white--text mb-2 text-center">Student of Systems Engineering</h2>
       <div class="mx-auto">
         <v-row justify="space-around">
-          <v-btn text dark href="https://www.linkedin.com/in/nicolaskobelt/">
+          <v-btn text dark href="https://www.linkedin.com/in/nicolaskobelt/" target="_blank">
             <v-icon dark>fab fa-linkedin</v-icon>
           </v-btn>
-          <v-btn text dark href="https://github.com/nicolaskobelt">
+          <v-btn text dark href="https://github.com/nicolaskobelt" target="_blank">
             <v-icon dark>fab fa-github</v-icon>
           </v-btn>
           <v-btn text dark href="mailto:nicolaskobelt@gmail.com">
             <v-icon dark>fas fa-envelope</v-icon>
           </v-btn>
-          <v-btn text dark href="https://t.me/nicolaskobelt">
+          <v-btn text dark href="https://t.me/nicolaskobelt" target="_blank">
             <v-icon dark>fab fa-telegram</v-icon>
           </v-btn>
         </v-row>
@@ -42,16 +42,15 @@
           <v-container>
             <v-layout align-center>
               <v-flex class="text-justify">
-                <v-card-text class="text-justify">
-                  I am student in the last year of systems engineering,
-                  I love this career, this help me found what is my passion.
-                  <br />I came from a little town in Córdoba, Argentina.
-                  Actually I am living in Córdoba City.
-                  What I said, the career help me found what are my passion,
-                  this are Artificial Intelligence, Data Sciene, App develop, etc.
-                  I can work in teams, I like know new people,
-                  learn from them and try understand each one.
-                  I like do fisical activities, go out with friends, enjoy.
+                <v-card-text
+                  v-resize-text="{ratio:1.5, minFontSize: '5px', maxFontSize: '20px', delay: 200}"
+                >
+                  I am a student of the Systems Engineering degree, I am in the last year.
+                  In this career I managed to affirm my passions for the development of technology,
+                  I love discovering new things and taking time to learn them in order to improve every day.
+                  I like to work in a team, to meet new people so I can also learn new things,
+                  not only technology, other cultures, customs, etc.
+                  On the other hand I like to leave the virtual world, enjoy nature, be with friends, family.
                 </v-card-text>
               </v-flex>
             </v-layout>
@@ -81,7 +80,9 @@
                 ></v-img>
               </v-list-item-avatar>
               <v-list-item-content>
-                <v-list-item-title class="headline">Software Developer</v-list-item-title>
+                <v-list-item-title
+                  v-resize-text="{ratio:1.5, minFontSize: '5px', maxFontSize: '20px', delay: 200}"
+                >Software Developer</v-list-item-title>
                 <v-list-item-subtitle>Checkbox Innovations</v-list-item-subtitle>
                 <v-list-item-subtitle>October 2019 - Present</v-list-item-subtitle>
               </v-list-item-content>
@@ -112,7 +113,9 @@
                 ></v-img>
               </v-list-item-avatar>
               <v-list-item-content>
-                <v-list-item-title class="headline">Systems Engineering</v-list-item-title>
+                <v-list-item-title
+                  v-resize-text="{ratio:1.5, minFontSize: '5px', maxFontSize: '20px', delay: 200}"
+                >Systems Engineering</v-list-item-title>
                 <v-list-item-subtitle>Universidad Católica de Córdoba</v-list-item-subtitle>
                 <v-list-item-subtitle>March 2015 - Present</v-list-item-subtitle>
               </v-list-item-content>
@@ -133,7 +136,9 @@
                 ></v-img>
               </v-list-item-avatar>
               <v-list-item-content>
-                <v-list-item-title class="headline">Project Management Diploma</v-list-item-title>
+                <v-list-item-title
+                  v-resize-text="{ratio:1.5, minFontSize: '5px', maxFontSize: '20px', delay: 200}"
+                >Project Management Diploma</v-list-item-title>
                 <v-list-item-subtitle>Universidad Católica de Córdoba</v-list-item-subtitle>
                 <v-list-item-subtitle>June 2019 - October 2019</v-list-item-subtitle>
               </v-list-item-content>
@@ -146,7 +151,9 @@
             <v-list-item justify="center">
               <v-list-item-avatar color="white"></v-list-item-avatar>
               <v-list-item-content>
-                <v-list-item-title class="headline">Bachelor in Economics and Management</v-list-item-title>
+                <v-list-item-title
+                  v-resize-text="{ratio:1.5, minFontSize: '5px', maxFontSize: '20px', delay: 200}"
+                >Bachelor in Economics and Management</v-list-item-title>
                 <v-list-item-subtitle>Instituto Parroquial San Juan Diego</v-list-item-subtitle>
                 <v-list-item-subtitle>March 2009 - December 2014</v-list-item-subtitle>
               </v-list-item-content>
@@ -157,25 +164,79 @@
     </section>
 
     <section>
-      <v-layout column >
+      <v-layout column>
         <v-flex xs12 sm4 class="my-5">
           <div class="text-center" id="skills">
             <v-icon class="cyan--text text--dark-2">fas fa-user-tag</v-icon>
             <h2 class="headline">Skills</h2>
           </div>
         </v-flex>
+
+        <v-col
+        cols="12"
+      >
+             <v-row
+          align="center"
+          justify="center">
+    
+          <v-card class="ma-3 pa-6" max-width="200" shaped="true"            
+            tile>
+            <v-card-text>
+              <div>Methodologies</div>
+              <p class="display-0 text--primary">Agile</p>
+              <p>Scrum, Kanban, XP</p>
+              <div class="text--primary">
+                From the university working
+                with agile methods.
+              </div>
+            </v-card-text>
+
+          </v-card>
+          <v-card class="ma-3 pa-6" max-width="200" shaped="true" tile>
+            <v-card-text>
+              <div>Technologies</div>
+              <p class="display-0 text--primary">Web Focused</p>
+              <p>Javascript, Typescript, more!</p>
+              <div class="text--primary">
+                When I started to learn to programming
+                the last technologies on the market was 
+                based on web/cloud this is why I'm working on these.
+              </div>
+            </v-card-text>
+          </v-card>          
+
+          <v-card class="ma-3 pa-6" max-width="200" shaped="true" tile>
+            <v-card-text>
+              <div>Desire to learn</div>
+              <p class="display-0 text--primary">Artificial Intelligence</p>
+              <p>N. Networks, ML, more!</p>
+              <div class="text--primary">
+                I love the power of computers to do human 
+                works.
+              </div>
+            </v-card-text>
+          </v-card>   
+  
+             </v-row>
+
+        </v-col>
         <div class="text-center">
-         <h4 > Technologic stack</h4>
+          <h4>Technologic stack</h4>
         </div>
-        <v-flex sm4>
-          <v-row justify="space-around">
-          <v-icon 
-          v-bind:key="icon" 
-          v-for="icon in icons"
-          large
-          color="grey lighten-1" >{{icon.name}}</v-icon>
-          </v-row>
-        </v-flex>
+        <v-container align="center">
+          <v-flex class="pa-2">
+            <v-row justify="space-around">
+              <v-icon
+                @mouseover="hover = true"
+                @mouseleave="hover = false"
+                v-bind:key="icon"
+                v-for="icon in icons"
+                large
+                color="grey ligthen-1"
+              >{{icon.name}}</v-icon>
+            </v-row>
+          </v-flex>
+        </v-container>
       </v-layout>
     </section>
   </v-content>
@@ -187,17 +248,18 @@ export default {
 
   data: () => ({
     icons: [
-      {'name': 'fab fa-angular'}, 
-      {'name': 'fab fa-docker'},
-      {'name': 'fab fa-vuejs'}, 
-      {'name': 'fab fa-node'},
-      {'name': 'fab fa-git-square'},
-      {'name': 'fab fa-jenkins'}, 
-      {'name': 'fab fa-js'}, 
-      {'name': 'fab fa-php'}, 
-      {'name': 'fab fa-python'}, 
-      {'name':'fab fa-sass'}
-    ]
+      { name: "fab fa-angular" },
+      { name: "fab fa-docker" },
+      { name: "fab fa-vuejs" },
+      { name: "fab fa-node" },
+      { name: "fab fa-git-square" },
+      { name: "fab fa-jenkins" },
+      { name: "fab fa-js" },
+      { name: "fab fa-php" },
+      { name: "fab fa-python" },
+      { name: "fab fa-sass" }
+    ],
+    hover: false
   })
 };
 </script>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
 import vuetify from './plugins/vuetify';
+import VueResizeText from 'vue-resize-text';
 
 Vue.config.productionTip = false
+
+Vue.use(VueResizeText);
 
 new Vue({
   el: '#app',
